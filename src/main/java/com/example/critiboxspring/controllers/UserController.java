f7c6509ab22054461dc972b5570de7eccbb7337a
Mateus Barcelos <chefinho2832@gmail.com>
2025-08-23 01:55:56 -0300

    fix: getbyid endpoint

--- src/main/java/com/example/critiboxspring/controllers/UserController.java
+++ src/main/java/com/example/critiboxspring/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -20,9 +21,21 @@ public class UserController {
         return service.getAllUsers().stream().map(UserDTO::new).toList();
     }
 
+
+    @PostMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id){
+        if(service.getUserById(id).isPresent()){
+                UserDTO user = new UserDTO(service.getUserById(id).get());
+                return new ResponseEntity<>(user,HttpStatus.FOUND);
+        }
+        else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO){
         User user = new User(userDTO);
         return new ResponseEntity<>(user,HttpStatus.CREATED);
     }
+
+
 }
--- src/main/java/com/example/critiboxspring/dto/UserDTO.java
+++ src/main/java/com/example/critiboxspring/dto/UserDTO.java
@@ -3,9 +3,10 @@ package com.example.critiboxspring.dto;
 import com.example.critiboxspring.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
-public record UserDTO (String username, List<ReviewDTO> reviews){
-    public UserDTO(User user){
-        this(user.getUsername(),user.getReviews().stream().map(ReviewDTO::new).toList());
+public record UserDTO (String username, List<ReviewDTO> reviews) {
+    public UserDTO(User user) {
+        this(user.getUsername(), user.getReviews().stream().map(ReviewDTO::new).toList());
     }
 }
