ae8dd03de5b2d0ad3f97ea19e8ffa275098f62c6
Mateus Barcelos <chefinho2832@gmail.com>
2025-08-23 01:18:11 -0300

    feat: user service

---
+++ src/main/java/com/example/critiboxspring/services/UserService.java
@@ -0,0 +1,31 @@
+package com.example.critiboxspring.services;
+
+import com.example.critiboxspring.model.User;
+import com.example.critiboxspring.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserService {
+    private UserRepository repository;
+
+    public UserService(UserRepository repository){
+        this.repository = repository;
+    }
+
+    public void saveUser(User user){
+        repository.save(user);
+    }
+
+    public void deleteUserById(Long id){
+        repository.deleteById(id);
+    }
+
+    public List<User> getAllUsers(){
+        return repository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id){
+        return repository.findById(id);
+    }
+}
